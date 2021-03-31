eb414c09d8fe530fd6b824b8be98cbf6e4d64888
jameshu1205 <jameshu1205@gmail.com>
2021-03-31 14:10:58 +0800

    edit

---
+++ 等腰直角三角形.py
@@ -0,0 +1,15 @@
+long = int (input('請輸入高'))
+i = 0
+
+while (i < long):
+    j = 0
+
+    while (j < i):
+        print ('*', end = " ")
+        j += 1
+
+    while (j == i):
+        print ('*')
+        j += 1
+    
+    i += 1    
